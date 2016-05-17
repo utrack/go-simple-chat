ed02c7731a74be10a1823ad3be6e7c6264c6327a
Nick K <u@colonpipe.biz>
2016-05-17 22:35:11 +0300

    Client: added incoming msgs' channel

--- client/client.go
+++ client/client.go
@@ -7,8 +7,12 @@ import (
 type Client interface {
 	// Send sends the message packet to the client. Returns err on error.
 	Send(message.One) error
-	// DisconChan returns a client's disconnection notification channel,
+	// DisconChan returns a client's disconnect notification channel,
 	// which receives single value (error) at most. Error contains the
 	// discon's description which describes why the client has gone away.
 	DisconChan() <-chan error
+
+	// MsgChan returns channel that receives incoming messages from the
+	// client.
+	MsgChan() <-chan message.One
 }
